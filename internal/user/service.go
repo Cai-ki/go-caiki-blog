d2f3d728b9352fbf014f4ef0eac164ffe8045976
Caiki <2875028086@qq.com>
2025-02-11 15:34:41 +0800

    实现了简单的注册逻辑

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,13 +1,18 @@
 package user
 
-import "github.com/Cai-ki/go-caiki-blog/models"
+import (
+	"crypto/sha256"
+
+	"github.com/Cai-ki/go-caiki-blog/models"
+	"github.com/Cai-ki/go-caiki-blog/pkg/storage"
+)
 
 type UserService interface {
-	Register(username, email, password string) (*models.User, error)
+	Register(username, email, password string) (models.Users, error)
 	// 返回 JWT Token
 	Login(email, password string) (string, error)
-	GetUserByEmail(email string) (*models.User, error)
-	GetUserByName(username string) (*models.User, error)
+	GetUserByEmail(email string) (models.Users, error)
+	GetUserByName(username string) (models.Users, error)
 }
 
 type userServiceImpl struct {
@@ -17,16 +22,33 @@ var _ UserService = (*userServiceImpl)(nil)
 
 var userService = userServiceImpl{}
 
-func (us userServiceImpl) Register(username, email, password string) (*models.User, error) {
-	return &models.User{}, nil
+func (us userServiceImpl) Register(username, email, password string) (user models.Users, err error) {
+	data := []byte(password)
+	hash := sha256.Sum256(data)
+
+	userInfo := models.Users{
+		Username: username,
+		Email:    email,
+		Password: string(hash[:]),
+	}
+
+	db := storage.DB.GetDB()
+	err = db.Create(&userInfo).Error
+	if err != nil {
+		return models.Users{}, err
+	}
+
+	db.Model(&models.Users{}).Where("username = ?", username).First(&user)
+
+	return user, nil
 }
 
 func (us userServiceImpl) Login(email, password string) (string, error) { return "", nil }
 
-func (us userServiceImpl) GetUserByEmail(email string) (*models.User, error) {
-	return &models.User{}, nil
+func (us userServiceImpl) GetUserByEmail(email string) (models.Users, error) {
+	return models.Users{}, nil
 }
 
-func (us userServiceImpl) GetUserByName(username string) (*models.User, error) {
-	return &models.User{}, nil
+func (us userServiceImpl) GetUserByName(username string) (models.Users, error) {
+	return models.Users{}, nil
 }
