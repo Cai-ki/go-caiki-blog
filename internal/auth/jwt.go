d83625b548afa82ad72e622ad9cb2232cab9fc43
Caiki <2875028086@qq.com>
2025-02-11 18:57:43 +0800

    实现了jwt登录验证的接口与中间件

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,1 +1,56 @@
 package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var JwtSecretKey = []byte("secret-key")
+
+type Claims struct {
+	jwt.RegisteredClaims
+	Username string `json:"username"`
+}
+
+type AuthJwt interface {
+	GenerateToken(username string) (string, error)
+	ParseToken(tokenString string) (*Claims, error)
+}
+
+type authJwtImpl struct {
+}
+
+var _ AuthJwt = (*authJwtImpl)(nil)
+
+var Jwt AuthJwt = authJwtImpl{}
+
+func (authJwtImpl) GenerateToken(username string) (string, error) {
+	claims := Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Issuer:    "go-caiki-blog",
+		},
+		Username: username,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(JwtSecretKey)
+}
+
+func (authJwtImpl) ParseToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return JwtSecretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, jwt.ErrSignatureInvalid
+}
--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -1,1 +1,37 @@
 package auth
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/Cai-ki/go-caiki-blog/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func JwtMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			utils.RespondWithJSON(c, http.StatusUnauthorized, "Unauthorized")
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenString == "" {
+			utils.RespondWithJSON(c, http.StatusUnauthorized, "Unauthorized")
+			c.Abort()
+			return
+		}
+
+		claims, err := Jwt.ParseToken(tokenString)
+		if err != nil {
+			utils.RespondWithJSON(c, http.StatusUnauthorized, "Unauthorized")
+			c.Abort()
+			return
+		}
+
+		c.Set("username", claims.Username)
+		c.Next()
+	}
+}
