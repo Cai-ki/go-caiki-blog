8d3231120473c60b747eca74fe3af408a5e4a664
Caiki <2875028086@qq.com>
2025-02-11 15:32:06 +0800

    修改了返回err的内容

--- utils/utils.go
+++ utils/utils.go
@@ -3,7 +3,7 @@ package utils
 import "github.com/gin-gonic/gin"
 
 func RespondWithError(c *gin.Context, code int, err error) {
-	c.JSON(code, err)
+	c.JSON(code, err.Error())
 }
 
 func RespondWithJSON(c *gin.Context, code int, data interface{}) {
