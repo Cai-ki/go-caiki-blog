f8a789885a1a89c4c6b30b4dcd2b4c54185d74d9
Caiki <2875028086@qq.com>
2025-02-11 23:02:28 +0800

    增添了post模块的路由

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,23 @@
 package routes
 
 import (
+	"github.com/Cai-ki/go-caiki-blog/internal/auth"
+	"github.com/Cai-ki/go-caiki-blog/internal/post"
 	users "github.com/Cai-ki/go-caiki-blog/internal/user"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
 	r.POST("/api/users", users.RegisterHandler)
-	r.POST("/api/auth/login", users.LoginHandler)
 	r.GET("/api/users/:username", users.GetUserHandler)
+	r.POST("/api/auth/login", users.LoginHandler)
+
+	authPosts := r.Group("/api/auth/posts")
+	authPosts.Use(auth.JwtMiddleware())
+	authPosts.POST("/", post.CreatePostHandler)
+	authPosts.PUT("/*title", post.UpdatePostHandler)
+	authPosts.DELETE("/*title", post.DeletePostHandler)
+
+	r.GET("/api/posts", post.ListPostsHandler)
+	r.GET("/api/posts/*title", post.GetPostHandler)
 }
