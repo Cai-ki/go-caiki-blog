9d688deaae13892f26de88122ea6adf6278b29ba
Caiki <2875028086@qq.com>
2025-02-11 15:33:09 +0800

    增添了GetDB方法

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -3,6 +3,7 @@ package storage
 import "gorm.io/gorm"
 
 type Storage interface {
+	GetDB() *gorm.DB
 }
 
 type storageImpl struct {
@@ -15,4 +16,11 @@ var DB Storage
 
 func SetupStorage(db *gorm.DB) {
 	DB = &storageImpl{db: db}
+}
+
+func (s *storageImpl) GetDB() *gorm.DB {
+	if s.db == nil {
+		panic("DB is not initialized")
+	}
+	return s.db
 }
