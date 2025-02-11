c8c4daa076625cb032a3a6d876c3f83b7537b724
Caiki <2875028086@qq.com>
2025-02-11 23:05:09 +0800

    添加了post模块的一些函数签名

--- internal/post/handler.go
+++ internal/post/handler.go
@@ -1,1 +1,13 @@
 package post
+
+import "github.com/gin-gonic/gin"
+
+func CreatePostHandler(c *gin.Context) {}
+
+func ListPostsHandler(c *gin.Context) {}
+
+func GetPostHandler(c *gin.Context) {}
+
+func UpdatePostHandler(c *gin.Context) {}
+
+func DeletePostHandler(c *gin.Context) {}
--- internal/post/service.go
+++ internal/post/service.go
@@ -1,1 +1,11 @@
 package post
+
+type PostService interface {
+}
+
+type postServiceImpl struct {
+}
+
+var _ PostService = (*postServiceImpl)(nil)
+
+var Service = postServiceImpl{}
