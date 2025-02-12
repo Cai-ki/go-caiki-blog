e5deb9e5a88d6328353dc386a03b9938ba3082c6
Caiki <2875028086@qq.com>
2025-02-12 18:18:22 +0800

    实现了两个handler逻辑

--- internal/post/handler.go
+++ internal/post/handler.go
@@ -1,10 +1,100 @@
 package post
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func CreatePostHandler(c *gin.Context) {}
+	"github.com/Cai-ki/go-caiki-blog/utils"
+	"github.com/gin-gonic/gin"
+)
 
-func ListPostsHandler(c *gin.Context) {}
+type createPostRequestInfo struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	// Tags    []string `json:"tags"`
+}
+
+type createPostResponseInfo struct {
+	ID        uint   `json:"id"`
+	Title     string `json:"title"`
+	UserID    uint   `json:"user_id"`
+	CreatedAt string `json:"created_at"`
+}
+
+func CreatePostHandler(c *gin.Context) {
+	var req createPostRequestInfo
+	if err := c.BindJSON(&req); err != nil {
+		utils.RespondWithError(c, http.StatusBadRequest, "Invalid request body")
+		return
+	}
+
+	userID := c.GetUint("user_id")
+
+	post, err := Service.CreatePost(userID, req.Title, req.Content)
+	if err != nil {
+		utils.RespondWithError(c, http.StatusInternalServerError, "Failed to create post")
+		return
+	}
+
+	res := createPostResponseInfo{
+		ID:        post.ID,
+		Title:     post.Title,
+		UserID:    post.UserID,
+		CreatedAt: post.CreatedAt.Format("2006-01-02 15:04:05"),
+	}
+
+	utils.RespondWithJSON(c, http.StatusCreated, res)
+}
+
+type listPostsRequestInfo struct {
+	Page  int `form:"page"`
+	Limit int `form:"limit"`
+}
+
+type listPostsResponseInfo struct {
+	Posts []postInfo `json:"posts"`
+}
+
+type postInfo struct {
+	ID     uint   `json:"id"`
+	Title  string `json:"title"`
+	Author struct {
+		ID   uint   `json:"id"`
+		Name string `json:"name"`
+	} `json:"author"`
+	CreatedAt string `json:"created_at"`
+}
+
+func ListPostsHandler(c *gin.Context) {
+	var req listPostsRequestInfo
+	if err := c.BindQuery(&req); err != nil {
+		utils.RespondWithError(c, http.StatusBadRequest, "Invalid request query")
+		return
+	}
+
+	posts, err := Service.ListPosts(req.Page, req.Limit)
+	if err != nil {
+		utils.RespondWithError(c, http.StatusInternalServerError, "Failed to list posts")
+		return
+	}
+
+	res := listPostsResponseInfo{
+		Posts: make([]postInfo, len(posts)),
+	}
+
+	for i, post := range posts {
+		res.Posts[i] = postInfo{
+			ID:    post.ID,
+			Title: post.Title,
+			Author: struct {
+				ID   uint   `json:"id"`
+				Name string `json:"name"`
+			}{post.UserID, post.User.Username},
+			CreatedAt: post.CreatedAt.Format("2006-01-02 15:04:05"),
+		}
+	}
+
+	utils.RespondWithJSON(c, http.StatusOK, res)
+}
 
 func GetPostHandler(c *gin.Context) {}
 
--- internal/post/service.go
+++ internal/post/service.go
@@ -1,6 +1,13 @@
 package post
 
+import (
+	"github.com/Cai-ki/go-caiki-blog/models"
+	"github.com/Cai-ki/go-caiki-blog/pkg/storage"
+)
+
 type PostService interface {
+	CreatePost(user_id uint, title string, content string) (res models.Posts, err error)
+	ListPosts(page int, limit int) (res []models.Posts, err error)
 }
 
 type postServiceImpl struct {
@@ -9,3 +16,37 @@ type postServiceImpl struct {
 var _ PostService = (*postServiceImpl)(nil)
 
 var Service = postServiceImpl{}
+
+func (postServiceImpl) CreatePost(user_id uint, title string, content string) (res models.Posts, err error) {
+	post := models.Posts{UserID: user_id, Title: title, Content: content}
+
+	db := storage.DB.GetDB()
+	if err = db.Create(&post).Error; err != nil {
+		return models.Posts{}, err
+	}
+
+	db.Model(&models.Posts{}).Where("user_id = ?", user_id).First(&res)
+
+	if res.ID == 0 {
+		return models.Posts{}, err
+	}
+
+	return res, nil
+}
+
+func (postServiceImpl) ListPosts(page int, limit int) (res []models.Posts, err error) {
+	if page <= 0 {
+		page = 1
+	}
+	if limit <= 0 {
+		limit = 10
+	}
+
+	db := storage.DB.GetDB()
+	db = db.Preload("User").Model(&models.Posts{})
+	if err = db.Offset((page - 1) * limit).Limit(limit).Find(&res).Error; err != nil {
+		return []models.Posts{}, err
+	}
+
+	return res, nil
+}
