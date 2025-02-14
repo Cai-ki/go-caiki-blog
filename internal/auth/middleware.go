fdb16ec149a46138bb29916d69d97c0850fd6ae5
Caiki <2875028086@qq.com>
2025-02-15 01:34:39 +0800

    修改了错误返回格式

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -13,21 +13,21 @@ func JwtMiddleware() gin.HandlerFunc {
 		// 注意写前端时，请求头Authorization的格式为Bearer tokenString
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			utils.RespondWithJSON(c, http.StatusUnauthorized, "Unauthorized")
+			utils.RespondWithError(c, http.StatusUnauthorized, "Unauthorized")
 			c.Abort()
 			return
 		}
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 		if tokenString == "" {
-			utils.RespondWithJSON(c, http.StatusUnauthorized, "Unauthorized")
+			utils.RespondWithError(c, http.StatusUnauthorized, "Unauthorized")
 			c.Abort()
 			return
 		}
 
 		claims, err := Jwt.ParseToken(tokenString)
 		if err != nil {
-			utils.RespondWithJSON(c, http.StatusUnauthorized, "Unauthorized")
+			utils.RespondWithError(c, http.StatusUnauthorized, "Unauthorized")
 			c.Abort()
 			return
 		}
@@ -35,11 +35,11 @@ func JwtMiddleware() gin.HandlerFunc {
 		ok, err := Jwt.ValidateClaimsExists(claims)
 
 		if err != nil {
-			utils.RespondWithJSON(c, http.StatusInternalServerError, "Internal Server Error")
+			utils.RespondWithError(c, http.StatusInternalServerError, "Internal Server Error")
 		}
 
 		if !ok {
-			utils.RespondWithJSON(c, http.StatusUnauthorized, "Unauthorized")
+			utils.RespondWithError(c, http.StatusUnauthorized, "Unauthorized")
 			c.Abort()
 			return
 		}
